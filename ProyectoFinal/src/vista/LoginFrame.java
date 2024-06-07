fe535b254b2eeb88de10ad49421b01c781877ff4
Sebastian Guerrero <seguerr10@gmail.com>
2024-06-06 19:11:07 -0500

    feat: gif backgrounds

--- ProyectoFinal/src/vista/LoginFrame.java
+++ ProyectoFinal/src/vista/LoginFrame.java
@@ -1,97 +1,137 @@
 package vista;
+
 import javax.swing.*;
 import controlador.LoginControl;
+import java.net.URL;
+import java.util.List;
+import java.util.Arrays;
 
 public class LoginFrame extends JFrame {
-  private JTextField emailField;
-  private JPasswordField passwordField;
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JLabel bannerLabel;
+    private List<String> imageUrls;
+    private int currentImageIndex;
 
-  public LoginFrame() {
-    super("Inicio de Sesión - MyHotel");
+    public LoginFrame() {
+        super("Inicio de Sesión - MyHotel");
 
-    JPanel loginPanel = new JPanel();
-    GroupLayout layout = new GroupLayout(loginPanel);
-    loginPanel.setLayout(layout);
-    layout.setAutoCreateGaps(true);
-    layout.setAutoCreateContainerGaps(true);
+        imageUrls = Arrays.asList(
+            "https://i.pinimg.com/originals/06/a8/5b/06a85b703ccc50fcc2214bac56214f48.gif",
+            "https://i.pinimg.com/originals/cb/10/99/cb109999e76292b5288868a61c043b90.gif",
+            "https://i.redd.it/1asyzovvrn8a1.gif" // URL de un GIF
+        );
+        currentImageIndex = 0;
 
-    JLabel emailLabel = new JLabel("Correo:");
-    JLabel passwordLabel = new JLabel("Contraseña:");
-    emailField = new JTextField(20);
-    passwordField = new JPasswordField(20);
-    JButton loginButton = new JButton("Iniciar Sesión");
-    JButton registerButton = new JButton("Registrarse");
+        JPanel loginPanel = new JPanel();
+        GroupLayout layout = new GroupLayout(loginPanel);
+        loginPanel.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
 
-    loginButton.addActionListener(e -> {
-      String email = emailField.getText();
-      String password = new String(passwordField.getPassword());
-      if (email.isEmpty() || password.isEmpty()) {
-        JOptionPane.showMessageDialog(LoginFrame.this, "Por favor, complete todos los campos", "Campos Vacíos",
-            JOptionPane.WARNING_MESSAGE);
-      } else {
-        if (email.equals("admin@admin.com") && password.equals("admin")) {
-          JOptionPane.showMessageDialog(LoginFrame.this, "Inicio de sesión como Administrador", "¡Bienvenido!",
-              JOptionPane.INFORMATION_MESSAGE);
-          dispose();
-          new AdminFrame();
-        } else {
-          boolean access = LoginControl.login(email, password);
-          if (access) {
-            JOptionPane.showMessageDialog(LoginFrame.this, "Inicio de sesión exitoso", "¡Bienvenido!",
-                JOptionPane.INFORMATION_MESSAGE);
+        bannerLabel = new JLabel();
+        updateBannerImage();
+
+        JLabel emailLabel = new JLabel("Correo:");
+        JLabel passwordLabel = new JLabel("Contraseña:");
+        emailField = new JTextField(20);
+        passwordField = new JPasswordField(20);
+        JButton loginButton = new JButton("Iniciar Sesión");
+        JButton registerButton = new JButton("Registrarse");
+        JButton changeBackgroundButton = new JButton("Cambiar Fondo");
+
+        loginButton.addActionListener(e -> {
+            String email = emailField.getText();
+            String password = new String(passwordField.getPassword());
+            if (email.isEmpty() || password.isEmpty()) {
+                JOptionPane.showMessageDialog(LoginFrame.this, "Por favor, complete todos los campos", "Campos Vacíos",
+                        JOptionPane.WARNING_MESSAGE);
+            } else {
+                if (email.equals("admin@admin.com") && password.equals("admin")) {
+                    JOptionPane.showMessageDialog(LoginFrame.this, "Inicio de sesión como Administrador", "¡Bienvenido!",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    dispose();
+                    new AdminFrame();
+                } else {
+                    boolean access = LoginControl.login(email, password);
+                    if (access) {
+                        JOptionPane.showMessageDialog(LoginFrame.this, "Inicio de sesión exitoso", "¡Bienvenido!",
+                                JOptionPane.INFORMATION_MESSAGE);
+                        dispose();
+                        new HomeFrame();
+                    } else {
+                        JOptionPane.showMessageDialog(LoginFrame.this, "Credenciales incorrectas", "Error de Inicio de Sesión",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
+
+        registerButton.addActionListener(e -> {
             dispose();
-            new HomeFrame();
-          } else {
-            JOptionPane.showMessageDialog(LoginFrame.this, "Credenciales incorrectas", "Error de Inicio de Sesión",
-                JOptionPane.ERROR_MESSAGE);
-          }
+            RegisterFrame registerScreen = new RegisterFrame();
+            registerScreen.setVisible(true);
+        });
+
+        changeBackgroundButton.addActionListener(e -> {
+            currentImageIndex = (currentImageIndex + 1) % imageUrls.size();
+            updateBannerImage();
+        });
+
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                        .addComponent(bannerLabel)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(emailLabel)
+                                        .addComponent(passwordLabel))
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addComponent(emailField)
+                                        .addComponent(passwordField)))
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(loginButton)
+                                .addComponent(registerButton))
+                        .addComponent(changeBackgroundButton)
+        );
+
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                        .addComponent(bannerLabel)
+                        .addGap(30)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(emailLabel)
+                                .addComponent(emailField))
+                        .addGap(20)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(passwordLabel)
+                                .addComponent(passwordField))
+                        .addGap(20)
+                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(loginButton)
+                                .addComponent(registerButton))
+                        .addGap(20)
+                        .addComponent(changeBackgroundButton)
+        );
+
+        add(loginPanel);
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void updateBannerImage() {
+        try {
+            URL url = new URL(imageUrls.get(currentImageIndex));
+            ImageIcon bannerIcon = new ImageIcon(url);
+            bannerLabel.setIcon(bannerIcon);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-      }
-    });
+    }
 
-    registerButton.addActionListener(e -> {
-      dispose();
-      RegisterFrame registerScreen = new RegisterFrame();
-      registerScreen.setVisible(true);
-    });
-
-    layout.setHorizontalGroup(
-        layout.createParallelGroup(GroupLayout.Alignment.CENTER)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(emailLabel)
-                    .addComponent(passwordLabel))
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(emailField)
-                    .addComponent(passwordField)))
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(loginButton)
-                .addComponent(registerButton)));
-
-    layout.setVerticalGroup(
-        layout.createSequentialGroup()
-            .addGap(50)
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                .addComponent(emailLabel)
-                .addComponent(emailField))
-            .addGap(20)
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                .addComponent(passwordLabel)
-                .addComponent(passwordField))
-            .addGap(20)
-            .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                .addComponent(loginButton)
-                .addComponent(registerButton)));
-
-    add(loginPanel);
-
-    setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    pack();
-    setLocationRelativeTo(null);
-    setVisible(true);
-  }
-
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(() -> new LoginFrame());
-  }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new LoginFrame());
+    }
 }
